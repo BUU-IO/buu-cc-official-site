8fc1ff02b3726110788eceb974bc4dab9615ef3e
MuWinds <muwinds@qq.com>
2025-04-28 15:11:41 +0800

    Update:新增Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/Banner";
 import { Metadata } from "next";
 import Footer from "@/components/Footer";
+import DoThingsBanner from "@/components/DoThingsBanner";
 export const metadata: Metadata = {
   title: "北京联合大学计算机社",
   description: "欢迎访问北京联合大学计算机社官方网站！",
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <main>
       <Banner />
+      <DoThingsBanner />
       <Footer />
       {/* 其他页面内容 */}
     </main>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,29 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-4">
-        <div className="text-center">
-          <p className="text-sm text-gray-400">
+        <div className="flex flex-col md:flex-row justify-center items-center">
+          <p className="text-sm text-center text-gray-400">
             Copyright © 2015-{currentYear} 北京联合大学计算机社 版权所有
           </p>
-          <p className="text-sm text-gray-400">
+        </div>
+        <div className="flex flex-col items-center">
+          <p className="text-sm text-gray-400 md:text-left mb-6 md:mb-0">
             感谢北京辰帆科技有限公司与北京熵擎科技有限公司为本社团提供的资金和服务器支持！
           </p>
-          <Image
-            src="https://www.horain.net/logo/HoRain-White-100.png"
-            alt="Sponsor Logo"
-            width={175}
-            height={39}
-            className="mx-auto mt-4"
-          />
+          <Link href="https://www.horain.net">
+            <Image
+              src="https://www.horain.net/logo/HoRain-White-100.png"
+              alt="Sponsor Logo"
+              width={175}
+              height={39}
+              className="mx-auto mt-4"
+            />
+          </Link>
           <Image
             src="SengineTechLogo.svg"
             alt="Sponsor Logo"
